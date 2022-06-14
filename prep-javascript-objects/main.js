a8d8e6e386de7539bceb5018f7f1d3efae4d8fab
Alvin Cho <alvinxcho@gmail.com>
2022-06-14 20:49:16 +0000

    Completed prep-javascript-objects execercise.

---
+++ prep-javascript-objects/main.js
@@ -0,0 +1,19 @@
+var person = {
+  firstName: 'Alvin',
+  lastName: 'Cho',
+  hobby: 'Gaming'
+};
+
+console.log(person);
+
+var fullName = person.firstName + ' ' + person.lastName;
+console.log(fullName);
+
+person.job = 'Web Developer';
+console.log(person.job);
+
+person.previousJob = 'Accountant';
+
+console.log(person.previousJob);
+
+console.log(person);
